f1479ae2237dc4f880cae9fcf3bdc929d76973b9
Daniel Papp <daniel.papp@gamesys.co.uk>
2013-01-21 18:35:32 +0000

    Some logic modification and some more comments to the code

--- control.py
+++ control.py
@@ -1,19 +1,59 @@
 from sublime_plugin import WindowCommand
 
+
 LEFT, TOP, RIGHT, BOTTOM = (0,1,2,3)
+
+dir_trans = {
+	'left': LEFT,
+	'up': TOP,
+	'right': RIGHT,
+	'down': BOTTOM
+}
+
+cell_values = [
+	(1, 3, RIGHT, LEFT),
+	(0, 2, BOTTOM, TOP),
+	(1, 3, LEFT, RIGHT),
+	(0, 2, TOP, BOTTOM)
+]
+
+
+"""
+One can get the window layout with calling get_layout
+on a sublime.Window object.
+
+The return value is a dictionary with 3 keys:
+
+- rows: A list of floats in [0,1] which defines where the
+				row the row cuts in the view are. More on cuts in
+				the description of cells.
+- cols: Same as rows just for columns..
+- cells: A list of 4 element list each of which define a cell.
+				 The 4 elements are pointers to the cut arrays and they
+				 define the cell geometry by [start_x, start_y, end_x, end_y]
+				 pointer order.
+"""
+
+# TODO: Create a stack which can store view changing commands, so we can go back an forth in the view change history.
+
+
+class BaseViewCommand(WindowCommand):
+
+	def run(self, direction)
 
 
 class MoveViewCommand(WindowCommand):
 	def run(self, direction):
+		# TODO: raise some bad direction error if direction not in dir_trans
 		cview = self.window.active_view()
 		cgroup = self.active_group()
 		layout = self.window.get_layout()
 
-		ngroup = self.get_best_view(cgroup, layout, direction)
+		ngroup = self.get_best_group(cgroup, layout, dir_trans[direction])
 		self.window.set_view_index(cview, ngroup, 0)
 		self.window.focus_view(cview)
 
-	def get_best_view(self, cgroup, layout, direction):
+	def get_best_group(self, cgroup, layout, direction):
 		""" Gets the most natural group on the border perpendicular to the moving direction.
 
 		By most natural I mean the group which have the largest intersection with the current
@@ -23,17 +63,10 @@ class MoveViewCommand(WindowCommand):
 		cl = len(cells)
 		ccell = cells[cgroup]
 
-		# TODO: Maybe we should filter down the fields 
-		if direction == 'down':
-			icells = ((cell[0], cell[2], i) for i in xrange(cl) if cells[i][TOP] >= ccell[BOTTOM] and cells[i] != ccell)
-		elif direction == 'up':
-			icells = ((cell[0], cell[2], i) for i in xrange(cl) if cells[i][BOTTOM] <= ccell[TOP] and cells[i] != ccell)
-		elif direction == 'left':
-			icells = ((cell[1], cell[3], i) for i in xrange(cl) if cells[i][RIGHT] <= ccell[LEFT] and cells[i] != ccell)
-		else: # direction == 'right'
-			icells = ((cell[1], cell[3], i) for i in xrange(cl) if cells[i][LEFT] >= ccell[RIGHT] and cells[i] != ccell)
+		first, second, other, this = cell_values[direction]
+		icells = ((cell[first], cell[second], i) for i in xrange(cl) if cells[i][other] == ccell[this] and cells[i] != ccell)
 
-		if direction in ['down', 'up']:
+		if direction in [TOP, BOTTOM]:
 			stuff = layout['cols']
 			crange = [stuff[ccell[0]], stuff[ccell[2]]]
 		else:
@@ -43,6 +76,9 @@ class MoveViewCommand(WindowCommand):
 		return self.get_best_intersection(cgroup, crange, icells, stuff)
 
 	def get_best_intersection(self, cindex, current, others, distribution):
+		# TODO: Move this function out of the class since it's used by other calsses as well
+		# 			or create a baseclass and subclass that for all commands.
+
 		best_value = 0.0
 		best_index = cindex
 		clow, chigh = current
@@ -56,13 +92,13 @@ class MoveViewCommand(WindowCommand):
 		return best_index
 
 
-class CreatePaneCommand(WindowCommand):
+class CreateViewCommand(WindowCommand):
 	def run(self, direction):
 		pass
 
 
-class MergePaneCommand(WindowCommand):
-	def run(self):
+class MergeViewCommand(WindowCommand):
+	def run(self, direction):
 		pass
 
 class MoveFocusCommand(WindowCommand):
